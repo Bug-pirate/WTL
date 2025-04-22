cace183096c2db9b66815fccb8d19c29df34df6a
Bug-pirate <patiladitya914@gmail.com>
2025-04-22 10:45:04 +0530

    Assignment 5

---
+++ Assignment_5/Employee.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Emp Id : </td><input type="number" name="t1" value="">
+        </tr>
+        <tr>
+            <td>Emp Name : </td><input type="text" name="t2" value="">
+        </tr>
+        <tr>
+            <td>Phone No : </td><input type="text" name="t3" value="">
+        </tr>
+        <tr>
+            <td>Department : </td><input type="text" name="t4" value="">
+        </tr>
+        <tr>
+            <td>Salary : </td><input type="text" name="t5" value="">
+        </tr>
+    </table>
+    <button>Add</button>
+    <button>Update</button>
+    <button>Delete</button>
+    <button>Display</button>
+</body>
+</html>
